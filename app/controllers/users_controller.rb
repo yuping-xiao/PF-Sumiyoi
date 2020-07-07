e44d295072cf77ddeb7e618e5c9a136739017a59
vagrant <vagrant@localhost.localdomain>
2020-07-07 08:18:59 +0000

    Finished User's uploading image function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,26 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  def edit
+  	@user = User.find(params[:id])
+  	if @user != current_user
+  		redirect_to user_path(current_user.id)
+  	end
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		flash[:notice] = "変更が保存されました！"
+  		redirect_to user_path(@user.id)
+  	else
+  		render 'edit'
+  	end
+  end
+
+  private
+  def user_params
+  		params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_132643) do
+ActiveRecord::Schema.define(version: 2020_07_07_074622) do
 
   create_table "room_images", force: :cascade do |t|
     t.string "image_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_07_05_132643) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.text "introduction"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
